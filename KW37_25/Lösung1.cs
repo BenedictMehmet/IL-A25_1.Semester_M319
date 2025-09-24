03ec9fbd8e9004e3c72ef68c526afd820d3413cd
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2025-09-24 10:20:58 +0200

    add new classes to tasks

---
+++ KW37_25/Lösung1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL_A25_1.Semester_M319.KW37_25
+{
+
+    // AUFGABE 1:
+    // Erzeuge zwei Zufallszahlen zwischen 1 und 100.
+    // Vergleiche die Zahlen:
+    //    Wenn die erste grösser ist -> gib aus: "Die erste Zahl ist grösser."
+    //    Wenn die zweite grösser ist -> gib aus: "Die zweite Zahl ist grösser."
+    //    Wenn beide gleich sind -> gib aus: "Die Zahlen sind gleich."
+
+
+    internal class Lösung1
+    {
+        static void Main(string[]args)
+        {
+
+            Random zufallZahl = new Random();
+
+            int zahl1 = zufallZahl.Next(1, 101);
+            int zahl2 = zufallZahl.Next(1, 101);
+
+
+            if (zahl1 > zahl2)
+            {
+                Console.WriteLine("Die erste Zahl ist grösser.");
+            }
+            else if (zahl1 < zahl2)
+            {
+                Console.WriteLine("Die zweite Zahl ist grösser.");
+            }
+            else
+            {
+                Console.WriteLine("Die Zahlen sind gleich");
+            }
+        }
+    }
+}
---
+++ KW37_25/Lösung2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL_A25_1.Semester_M319.KW37_25
+{
+    // AUFGABE 2:
+    // Deklariere zwei Variablen "punkteSpieler" und "punkteComputer" in int.
+    // Addiere jeweils einen Wert (z.B. 5 oder 10 Punkte) dazu.
+    // Prüfe danach:
+    //    Wenn punkteSpieler >= 50 -> gib aus: "Spieler hat gewonnen!"
+    //    Wenn punkteComputer >= 50 -> gib aus: "Computer hat gewonnen!"
+    //    Sonst -> gib aus: "Noch keiner hat gewonnen."
+
+
+    internal class Lösung2
+    {
+        static void Main(string[] args)
+        {
+
+            int punkteSpieler = 40;
+            int punkteComputer = 45;
+
+
+            punkteSpieler += 10;
+            punkteComputer += 5;
+
+
+            Console.WriteLine($"Spielerpunkte {punkteSpieler}");
+            Console.WriteLine($"Spielerpunkte {punkteComputer}");
+
+
+            if (punkteSpieler >= 50)
+            {
+                Console.WriteLine("Spieler hat gewonnen");
+            }
+            else if (punkteComputer >= 50)
+            {
+                Console.WriteLine("Computer hat gewonnen");
+            }
+            else
+            {
+                Console.WriteLine("Niemand hat gewonnen");
+            }
+
+
+
+
+        }
+    }
+}
