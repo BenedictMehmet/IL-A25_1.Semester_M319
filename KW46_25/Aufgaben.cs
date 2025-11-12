358b5480a247343aeb9f6427ff10018020769f48
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2025-11-12 10:38:13 +0100

    add more classes

---
+++ KW46_25/Aufgaben.cs
@@ -0,0 +1,53 @@
+﻿using IL_A25_1.Semester_M319.KW37_25;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL_A25_1.Semester_M319.KW46_25
+{
+    internal class Aufgaben
+    {
+
+
+        /* 
+     Aufgabe 1:
+
+     Schreibe ein Programm, das die Temperatur über eine bestimmte Anzahl Tage simuliert.
+
+     Der Benutzer gibt ein, wie viele Tage simuliert werden sollen.
+     Für jeden Tag wird eine zufällige Temperatur zwischen -10°C und +35°C erzeugt.
+
+     Danach soll das Programm:
+     - Alle generierten Temperaturen anzeigen
+     - Die höchste Temperatur anzeigen
+     - Die tiefste Temperatur anzeigen
+     - Die Durchschnittstemperatur berechnen und ausgeben
+
+     Tipp: Verwende eine zusätzliche Methode, um den Durchschnitt zu berechnen.
+     */
+
+
+
+        /*
+    Aufgabe 2:
+    -------------------
+    Erstelle ein Programm, das Zufallsnoten für eine bestimmte Anzahl Schüler simuliert.
+
+    Der Benutzer gibt ein, wie viele Schüler simuliert werden sollen.
+    Für jeden Schüler wird eine Punktzahl zwischen 0 und 100 generiert.
+
+    Danach soll das Programm:
+    - Alle Noten anzeigen
+    - Den besten Schüler (höchste Punktzahl) anzeigen
+    - Den schwächsten Schüler (tiefste Punktzahl) anzeigen
+    - Den Durchschnitt der Klasse berechnen und anzeigen
+
+    Verwende eine eigene Methode, um den Durchschnitt zu berechnen.
+    */
+
+
+
+    }
+}
---
+++ KW46_25/WetterSimulation2.cs
@@ -0,0 +1,115 @@
+﻿using IL_A25_1.Semester_M319.KW37_25;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL_A25_1.Semester_M319.KW46_25
+{
+    internal class WetterSimulation2
+    {
+        static void Main(string[]args)
+        {
+
+        }
+    }
+
+
+
+
+    class Wetter1
+    {
+        //Datentyp deklarien, dass die Anzahl Tage angezeigt werden kann.
+        public int days;
+
+        // Zufallszahlgenerator codieren. Ich brauche die Bibliothek dazu
+        public Random r = new Random();
+
+        public Wetter1()
+        {
+            Console.WriteLine("Wie viele Tage sollen simuliert werden?");
+            days = int.Parse(Console.ReadLine());
+        }
+
+        // Hauptmethode für die Simulation
+        public void RunSimulation()
+        {
+            // Array für tägliche Temperaturen.
+            int[] temperatur = new int[days];
+
+            // Mögliche Wetterbedingungen
+            string[] möglicheBedingungen = {"Sonnig", "Bewölk", "Regen", "Schnee" };
+
+            // Array für die zufällig gewählten Wetterbedingungen der einzelnen Tage
+            string[] wetterBedingungen = new string[days];
+
+
+            // Schleife, die für jeden Tag eine Temperatur und eine mögliche Wetterbedingung zufällig generiert
+            for (int i = 0; i < days; i++)
+            {
+                // zufällige Temperatur zwischen -10 und 40 grad
+                temperatur[i] = r.Next(-10, 41);
+
+                // Zufällige Auswahl einer Wetterbedingung aus der Liste der möglichen Wetterbedingungen
+                wetterBedingungen[i] = möglicheBedingungen[r.Next(möglicheBedingungen.Length)];
+            }
+
+
+            double durschnitt = DurchschnittBerrechnen(temperatur);
+
+
+            Console.WriteLine($"{FindeHäufigsteBedingung(wetterBedingungen)}");
+        }
+
+        private double DurchschnittBerrechnen(int[] berechnen)
+        {
+            double sum = 0;
+
+            // Alle Temperature aufsummieren
+            for(int i = 0; i < berechnen.Length; i++)
+            {
+                sum += berechnen[i];
+            }
+
+            // Durschnitt berechnen (Summe / Anzahl Werte)
+            double durschnitt = sum / berechnen.Length;
+            return durschnitt;
+        }
+        
+
+        // Recherchiert diese Methode zu Hause. Zeile für Zeile. Was kann es sein und warum?
+
+        private string FindeHäufigsteBedingung(string[] bedingungen)
+        {
+            int zähler = 0;
+            string häufigste = bedingungen[0]; // Erste Wetterbedingung im Array
+
+
+            // Äussere Schleife: prüft jede Bedingung
+            for (int i = 0; i < bedingungen.Length; i++)
+            {
+                int tempZähler = 0;
+
+                // Innere Schleife: vergleicht die aktuelle Bedingungen mit allen anderen
+                for (int j = 0; j < bedingungen.Length; j++)
+                {
+                    if (bedingungen[j] == bedingungen[i])
+                    {
+                        tempZähler++;
+                    }
+                }
+
+                if (tempZähler > zähler)
+                {
+                    zähler = tempZähler;
+                    häufigste = bedingungen[i];
+                }
+            }
+
+            return häufigste;
+        }
+
+
+    }
+}
