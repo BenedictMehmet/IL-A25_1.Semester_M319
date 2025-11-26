75263dd643f06bda9d2887a3cceac2c76218ac3b
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2025-11-26 10:57:16 +0100

    difficult task

---
+++ KW48_25/AufgabeOptional.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL_A25_1.Semester_M319.KW48_25
+{
+    internal class AufgabeOptional
+    {
+                            /*
+                    Aufgabe: Einkaufs-/Bestellsystem mit Arrays
+
+                    1. Erstelle zwei Klassen: "Produkt" und "Warenkorb".
+
+                       Klasse Produkt:
+                       - Properties: Name (string), Preis (double), Lagerbestand (int)
+                       - Konstruktor: initialisiert alle Properties
+                       - Methode: Zeigt Produktinformationen an
+
+                       Klasse Warenkorb:
+                       - Property: Array von Produkten mit festgelegter maximaler Anzahl
+                       - Property: Array von Mengen für jedes Produkt
+                       - Methode: Produkt und Menge hinzufügen
+                       - Methode: Gesamtsumme berechnen
+                       - Methode: Warenkorb anzeigen
+
+                    2. Im Main:
+                       - Erstelle ein Array von Produkt-Objekten (z. B. 3 Produkte)
+                       - Erstelle ein Warenkorb-Objekt
+
+                    3. Implementiere eine do/while-Schleife, um Produkte auszuwählen:
+                       - Zeige alle Produkte mit Nummer, Name, Preis und Lagerbestand
+                       - Frage nach der Produktnummer und Menge
+                       - Prüfe mit if/else:
+                           * Ob die eingegebene Menge verfügbar ist
+                           * Falls nicht, Fehlermeldung ausgeben
+                           * Falls ja, Produkt und Menge zum Warenkorb hinzufügen und Lagerbestand reduzieren
+
+                    4. Schleife läuft, bis der Benutzer angibt, dass er nichts mehr kaufen möchte.
+
+                    5. Am Ende: Warenkorb anzeigen und Gesamtsumme berechnen
+                    */
+
+
+    }
+}
