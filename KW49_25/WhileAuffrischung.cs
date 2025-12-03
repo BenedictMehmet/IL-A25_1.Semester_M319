8a6262b504680430dc50ad086b5e3b492867023b
BenedictMehmet <Oezdemir.Mehmet@bene-edu.ch>
2025-12-03 11:10:13 +0100

    WhileAuffrischung Task

---
+++ KW49_25/WhileAuffrischung.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IL_A25_1.Semester_M319.KW49_25
+{
+    internal class WhileAuffrischung
+    {
+        // Aufgabe 1:
+        // Schreibe ein Programm, das den Benutzer nach einer Zahl fragt 
+        // und dann alle Zahlen von 0 bis zu dieser Zahl ausgibt. 
+        // Verwende dazu eine while-Schleife.
+
+        // Aufgabe 2:
+        // Schreibe ein Programm, das so lange "Life is good" ausgibt,
+        // bis der Benutzer "stop" eingibt. 
+        // Verwende eine while-Schleife und eine Variable für die Benutzereingabe.
+
+        // Aufgabe 3:
+        // Erstelle ein Ratespiel: Der Benutzer soll eine Zahl zwischen 1 und 10 erraten.
+        // Die Schleife läuft so lange, bis die richtige Zahl geraten wurde.
+    }
+}
